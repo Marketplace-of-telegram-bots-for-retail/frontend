f923e6948205ae60394d7618d0a5eab19ea9fcdd
Pavel Zakharov <trade93@gmail.com>
2023-09-23 23:32:20 -0500

    Apply styles

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,26 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Header from '../Header/Header';
 import Poster from '../Poster/Poster';
 import Title from '../Title/Title';
 import Content from '../Content/Content';
-import './App.css';
 import AuthButtons from '../AuthButtons/AuthButtons';
+import './App.css';
 
 const App = () => {
+  const [showAuthButtons, setShowAuthButtons] = useState(false);
+
   return (
     <Router>
       <div className='container'>
         <div className='inner-container'>
-          <Header />
+          <Header
+            showAuthButtons={showAuthButtons}
+            setShowAuthButtons={setShowAuthButtons}
+          />
           <main>
             <Poster />
             <Title />
             <Content />
           </main>
+          { showAuthButtons && <AuthButtons />}
         </div>
       </div>
-      <AuthButtons />
     </Router>
   );
 };
--- src/components/AuthModal/AuthModal.jsx
+++ src/components/AuthModal/AuthModal.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { ReactComponent as Close } from '../../images/close-icon.svg';
+import './AuthModal.css';
 
-const AuthModal = ({ onClose }) => {
-  const [isLogin, setIsLogin] = useState(true); // добавьте это состояние в AuthModal
+const AuthModal = ({ onClose, isLogin, setIsLogin }) => {
   const [userType, setUserType] = useState('Покупатель');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -25,41 +26,57 @@ const AuthModal = ({ onClose }) => {
   };
 
   return (
-    <div style={{ position: 'absolute', top: '10%', left: '10%', background: 'white', padding: '20px', borderRadius: '12px' }}>
-      <div>
-        <h2>{isLogin ? 'Войти' : 'Регистрация'}</h2>
-        <button type="button" onClick={onClose}>X</button>
-      </div>
-      <div>
-        <button type="button" onClick={() => setUserType('Покупатель')}>Покупатель</button>
-        <button type="button" onClick={() => setUserType('Продавец')}>Продавец</button>
-      </div>
-      <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Электронная почта" />
-      <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Пароль" />
-      {!isLogin && <input type="password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} placeholder="Повторите пароль" />}
-      <label htmlFor="rememberMe">
-        <input id="rememberMe" type="checkbox" checked={rememberMe} onChange={() => setRememberMe(!rememberMe)} />
-        Запомнить меня
-      </label>
-      <button type="submit" onClick={handleSubmit}>{isLogin ? 'Войти' : 'Зарегистрироваться'}</button>
-      {isLogin ? (
-        <div>
-          <Link to="/reset-password">Забыли пароль?</Link>
-          <span>
-            Нет аккаунта?
-            {' '}
-            <button type='submit' onClick={() => setIsLogin(false)}>Зарегистрироваться</button>
-          </span>
+    <div className='modal__container'>
+      <div className='modal__content'>
+        <h2 className='modal__title'>{isLogin ? 'Войти' : 'Регистрация'}</h2>
+        <Close className='modal__close' onClick={onClose} />
+        <div className='modal__inputs'>
+          <div className='modal__user-type-box'>
+            <button
+              className={`modal__user-type-btn ${userType === 'Покупатель' ? 'modal__user-type-btn_active' : 'modal__user-type-btn_inactive'}`}
+              type="button"
+              onClick={() => setUserType('Покупатель')}
+            >
+              Покупатель
+            </button>
+            <button
+              className={`modal__user-type-btn ${userType === 'Продавец' ? 'modal__user-type-btn_active' : 'modal__user-type-btn_inactive'}`}
+              type="button"
+              onClick={() => setUserType('Продавец')}
+            >
+              Продавец
+            </button>
+          </div>
+          <input className='modal__input' type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Электронная почта" />
+          <input className='modal__input' type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Пароль" />
+          {!isLogin && <input className='modal__input' type="password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} placeholder="Повторите пароль" />}
+          <div className='modal__remember-checkbox'>
+            <button type='button' className={`modal__remember-icon ${rememberMe ? 'modal__remember-icon_checked' : 'modal__remember-icon_blank'}`} onClick={() => setRememberMe(!rememberMe)} />
+            <span className='modal__span'>Запомнить меня</span>
+          </div>
         </div>
-      ) : (
-        <div>
-          <span>
-            Есть аккаунт?
-            {' '}
-            <button type='submit' onClick={() => setIsLogin(true)}>Войти</button>
-          </span>
-        </div>
-      )}
+        <button className='modal__submit-btn' type="submit" onClick={handleSubmit}>{isLogin ? 'Войти' : 'Зарегистрироваться'}</button>
+        {isLogin ? (
+          <>
+            <Link className='modal__span' to="/reset-password">Забыли пароль?</Link>
+            <div className='modal__toggle-form'>
+              <span className='modal__span'>
+                Нет аккаунта?
+                {' '}
+                <span className='modal__span modal__span_link' onClick={() => setIsLogin(false)}>Зарегистрироваться</span>
+              </span>
+            </div>
+          </>
+        ) : (
+          <div className='modal__toggle-form'>
+            <span className='modal__span'>
+              Есть аккаунт?
+              {' '}
+              <span className='modal__span modal__span_link' onClick={() => setIsLogin(true)}>Войти</span>
+            </span>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
