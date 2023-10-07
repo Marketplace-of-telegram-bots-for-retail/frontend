213c7328d50e790c5e19a0dc757fb8182a3a9cf6
OksanaKam <moremore1@yandex.ru>
2023-10-07 10:25:26 +0200

    [/] add functional on product page

--- src/components/ProductReviewInitial/ProductReviewInitial.jsx
+++ src/components/ProductReviewInitial/ProductReviewInitial.jsx
@@ -6,18 +6,29 @@ const ProductReviewInitial = () => {
   const [isShown, setIsShown] = useState(false);
 
   function handleFeedbackClick() {
-    setIsShown((current) => !current);
+    setIsShown(!isShown);
   }
+
+  function handleSendClick() {
+
+  }
+
   return (
     <>
       <div className='product__review-initial'>
-        <button className='product__review-open' type='button' onClick={handleFeedbackClick} aria-label='Оставить отзыв'>Оставить отзыв</button>
+        {isShown && (
+          <span className='product__review-question'>Вам понравился бот?</span>
+        )}
+        {!isShown && (
+          <button className='product__review-open' type='button' onClick={handleFeedbackClick} aria-label='Оставить отзыв'>Оставить отзыв</button>
+        )}
         <button className='product__review-show' type='button' aria-label='Показать все'>Показать все</button>
       </div>
       {isShown && (
         <div className='product__review-block'>
           <StarRating />
-          <input className='product__review-input' type='text' id='feedback' name='feedback' placeholder='сюда можно написать отзыв' autoComplete='off' />
+          <textarea className='product__review-input' type='text' id='feedback' name='feedback' placeholder='сюда можно написать отзыв' autoComplete='off' />
+          <button className='product__review-send' type='submit' onClick={handleSendClick} aria-label='Оставить отзыв'>Оставить отзыв</button>
         </div>
       )}
     </>
