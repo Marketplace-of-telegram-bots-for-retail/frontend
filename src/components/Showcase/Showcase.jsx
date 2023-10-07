e5bed3bee1d7b9891982502fade98254b716642c
Dmitry Tokmak <dimka.listok@gmail.com>
2023-10-07 19:04:29 +0300

    api

--- src/components/Showcase/Showcase.jsx
+++ src/components/Showcase/Showcase.jsx
@@ -3,8 +3,8 @@ import './Showcase.css';
 import { Dropdown } from '../Dropdown/Dropdown';
 import { Cards } from '../Cards/Cards';
 import { More } from '../More/More';
-import { cards } from '../../temp/cards';
 import { AftPoster } from '../AftPoster/AftPoster';
+import { cards } from '../../temp/cards';
 
 export const Showcase = () => {
   const handleSort = (value) => {
