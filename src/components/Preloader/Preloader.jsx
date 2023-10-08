1c298b34290abdde0d4a85f22e465f52e5f0ab05
Dmitry Tokmak <dimka.listok@gmail.com>
2023-10-08 19:26:41 +0300

    add peloader

---
+++ src/components/Preloader/Preloader.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Preloader.css';
+
+export const Preloader = () => {
+  return (
+    <div className="preloader">
+      <div className="preloader__container">
+        <span className="preloader__round"></span>
+      </div>
+    </div>
+  );
+};
