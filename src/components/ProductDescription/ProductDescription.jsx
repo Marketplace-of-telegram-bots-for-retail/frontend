9dc6982cd3722c60f39a23e3952cd8004de6268d
OksanaKam <moremore1@yandex.ru>
2023-10-12 09:09:42 +0200

    [/] add hide video

--- src/components/ProductDescription/ProductDescription.jsx
+++ src/components/ProductDescription/ProductDescription.jsx
@@ -3,13 +3,17 @@ import './ProductDescription.css';
 import testVideo from '../../images/video.jpg';
 
 const ProductDescription = ({ card }) => {
+  const { video } = card;
+
   return (
     <div className='product__description'>
       <p className="product__description-text">{card.description}</p>
-      <div className='product__description-videos'>
-        <h2 className='product__description-heading'>Видеообзор</h2>
-        <iframe className='product__description-link' title='product-video' src={card.video || testVideo} frameBorder='0' allowFullScreen></iframe>
-      </div>
+      {video !== '' && (
+        <div className='product__description-videos'>
+          <h2 className='product__description-heading'>Видеообзор</h2>
+          <iframe className='product__description-link' title='product-video' src={card.video || testVideo} frameBorder='0' allowFullScreen></iframe>
+        </div>
+      )}
     </div>
   );
 };
