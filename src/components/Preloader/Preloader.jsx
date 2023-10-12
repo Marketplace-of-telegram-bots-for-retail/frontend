52b445b27e6ce3c6c4646397386e64d9f91c1d43
Dmitry Tokmak <dimka.listok@gmail.com>
2023-10-12 16:00:38 +0300

    Preloader

--- src/components/Preloader/Preloader.jsx
+++ src/components/Preloader/Preloader.jsx
@@ -3,9 +3,11 @@ import './Preloader.css';
 
 export const Preloader = () => {
   return (
-    <div className="preloader">
-      <div className="preloader__container">
-        <span className="preloader__round"></span>
+    <div className='preloader'>
+      <div className='preloader__container'>
+        <div className='preloader__outer'></div>
+        <div className='preloader__middle'></div>
+        <div className='preloader__inner'></div>
       </div>
     </div>
   );
