666c5625d9c98e3e2f0d5ac11d973d4adb929f76
Ekaterina Shreyner <kkkatya88@yandex.ru>
2023-10-19 16:59:24 +0400

    feat: add component AboutBots

--- src/components/info/AboutMain/AboutMain.jsx
+++ src/components/info/AboutMain/AboutMain.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import './AboutMain.css';
 import ImageHello from '../../../images/pic_main-cart.png';
-// import ErrorPage from '../../ErrorPage/ErrorPage';
 
 const AboutMain = () => {
   return (
     <section className='content__about about' id='about'>
       <div className='about__container'>
         <h1 className='about__title'>О маркетплейсе</h1>
-
         <p className='about__subtitle'>Платформа, позволяющая ритейлерам найти и приобрести готовые телеграм-боты или заказать разработку индивидуального бота.</p>
         <ul className='about__list'>
           <li className='about__list-item'>экономия времени и ресурсов</li>
@@ -17,8 +15,6 @@ const AboutMain = () => {
         </ul>
       </div>
       <img className='about__image' src={ImageHello} alt='картинка с приветсвием'></img>
-
-      {/* <ErrorPage /> */}
     </section>
   );
 };
