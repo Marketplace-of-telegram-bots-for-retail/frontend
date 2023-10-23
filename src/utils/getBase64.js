151f696c4e2eacf83095b218ddae21c6143fc5ad
Anna <thoria@yandex.ru>
2023-10-23 18:17:30 +0300

    add getBase64 to utils

---
+++ src/utils/getBase64.js
@@ -0,0 +1,12 @@
+const getBase64 = (file, cb) => {
+  const reader = new FileReader();
+  reader.readAsDataURL(file);
+  reader.onload = function () {
+    cb(reader.result);
+  };
+  reader.onerror = function (error) {
+    console.log('Error: ', error);
+  };
+};
+
+export default getBase64;
