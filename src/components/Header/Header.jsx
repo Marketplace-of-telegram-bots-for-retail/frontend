89aa5f8c766bf5b028a0b3945e0bba1c9d0f38d1
Dmitry Tokmak <dimka.listok@gmail.com>
2023-10-25 09:56:38 +0300

    fix header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,11 +7,13 @@ import { ARR_NAV } from '../../utils/constants';
 import './Header.css';
 import logo from '../../images/logo-color.png';
 import { useForm } from '../../hooks/useForm';
+import { useScroll } from '../../hooks/useScroll';
 import { CurrentUserContext } from '../../contexts/currentUserContext';
 import { getProducts } from '../../store/dataProductsStateSlice';
 import { useQueryParameter } from '../../hooks/useQueryParameter';
 
 const Header = ({ setShowAuthButtons, isAuthorized, isPreloader }) => {
+  const { scrollPosition } = useScroll();
   const { formRequest } = useQueryParameter();
   const currentUser = useContext(CurrentUserContext);
   const { values, handleChange } = useForm();
@@ -33,78 +35,84 @@ const Header = ({ setShowAuthButtons, isAuthorized, isPreloader }) => {
     setLogin(!isLogin);
     setShowAuthButtons(true);
   };
-
+  console.log(scrollPosition);
   return (
     <section className='page__header header'>
-      <nav className='header__nav'>
-        {ARR_NAV.map((link, i) => {
-          return (
-            <HashLink key={i} to={link.link} className='header__link'>
-              {link.label}
-            </HashLink>
-          );
-        })}
-      </nav>
-      <div className='header__basis header__basis_sticky'>
-        <div className='header__navbar'>
-          <NavLink className='header__logo' to='/'>
-            <img src={logo} alt='логотип' />
-          </NavLink>
-          <NavLink className='header__button-medium' to='/'>
-            <span className='header__catalog-icon'></span>
-            <span className='header__catalog-text'>Каталог</span>
-          </NavLink>
-          <form className='header__search-form' onSubmit={handleSubmit}>
-            <input
-              type='search'
-              className='header__search-input'
-              placeholder='Искать бота'
-              value={values?.search || ''}
-              name='search'
-              onChange={handleChange}
-              onBlur={handleSubmit}
-            ></input>
-            <button
-              className='header__search-button'
-              type='submit'
-              placeholder='Искать'
-              disabled={isPreloader}
-            >
-              <span className='header__search-icon'></span>
-            </button>
-          </form>
-        </div>
-        <div className='header__navbar'>
-          <Link to='/cart' className='header__menu-button-icon'>
-            <span className='header__button-icon header__button-icon_cart'></span>
-            {total_amount > 0 ? (
-              <span className='header__badge-counter'>{total_amount}</span>
-            ) : null}
-            <span className='header__button-text'>Корзина</span>
-          </Link>
-          <Link to='/favorites' className='header__menu-button-icon'>
-            <span className='header__button-icon header__button-icon_favorite'></span>
-            {favoritesCount > 0 ? (
-              <span className='header__badge-counter'>{favoritesCount}</span>
-            ) : null}
-            <span className='header__button-text'>Избранное</span>
-          </Link>
-          {!isAuthorized ? (
-            <button
-              className='header__button-small header__button-small_border'
-              type='button'
-              onClick={() => handleLogin()}
-            >
-              Войти
-            </button>
-          ) : (
-            <Link to='/profile' className='header__menu-button-icon '>
-              <span className='header__button-icon header__button-icon_profile'></span>
-              <span className='header__button-text'>
-                {currentUser.first_name}
-              </span>
+      <div className='header__container'>
+        <nav className='header__nav'>
+          {ARR_NAV.map((link, i) => {
+            return (
+              <HashLink key={i} to={link.link} className='header__link'>
+                {link.label}
+              </HashLink>
+            );
+          })}
+        </nav>
+        <div
+          className={`header__basis ${
+            scrollPosition >= 140 ? 'header__basis_sticky' : ''
+          }`}
+        >
+          <div className='header__navbar'>
+            <NavLink className='header__logo' to='/'>
+              <img src={logo} alt='логотип' />
+            </NavLink>
+            <NavLink className='header__button-medium' to='/'>
+              <span className='header__catalog-icon'></span>
+              <span className='header__catalog-text'>Каталог</span>
+            </NavLink>
+            <form className='header__search-form' onSubmit={handleSubmit}>
+              <input
+                type='search'
+                className='header__search-input'
+                placeholder='Искать бота'
+                value={values?.search || ''}
+                name='search'
+                onChange={handleChange}
+                onBlur={handleSubmit}
+              ></input>
+              <button
+                className='header__search-button'
+                type='submit'
+                placeholder='Искать'
+                disabled={isPreloader}
+              >
+                <span className='header__search-icon'></span>
+              </button>
+            </form>
+          </div>
+          <div className='header__navbar'>
+            <Link to='/cart' className='header__menu-button-icon'>
+              <span className='header__button-icon header__button-icon_cart'></span>
+              {total_amount > 0 ? (
+                <span className='header__badge-counter'>{total_amount}</span>
+              ) : null}
+              <span className='header__button-text'>Корзина</span>
             </Link>
-          )}
+            <Link to='/favorites' className='header__menu-button-icon'>
+              <span className='header__button-icon header__button-icon_favorite'></span>
+              {favoritesCount > 0 ? (
+                <span className='header__badge-counter'>{favoritesCount}</span>
+              ) : null}
+              <span className='header__button-text'>Избранное</span>
+            </Link>
+            {!isAuthorized ? (
+              <button
+                className='header__button-small header__button-small_border'
+                type='button'
+                onClick={() => handleLogin()}
+              >
+                Войти
+              </button>
+            ) : (
+              <Link to='/profile' className='header__menu-button-icon '>
+                <span className='header__button-icon header__button-icon_profile'></span>
+                <span className='header__button-text'>
+                  {currentUser.first_name}
+                </span>
+              </Link>
+            )}
+          </div>
         </div>
       </div>
     </section>
