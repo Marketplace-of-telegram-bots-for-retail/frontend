6bd15b2bf12346937fe8acf86397df368b536bd0
Dmitry Tokmak <dimka.listok@gmail.com>
2023-11-02 11:01:42 +0300

    fix stre

--- src/components/profile/goods/GoodsNewBot/GoodsNewBot.jsx
+++ src/components/profile/goods/GoodsNewBot/GoodsNewBot.jsx
@@ -4,8 +4,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import './GoodsNewBot.css';
 import info from '../../../../images/Help.svg';
 import { CATEGORY_OPTIONS } from '../../../../utils/constants';
-import { setCategories } from '../../../../store/dataSearchFormSlice';
 import { getSearchFormData } from '../../../../store';
+import { setCategories } from '../../../../store/searchFormDataSlice';
 
 const GoodsNewBot = () => {
   const dispatch = useDispatch();
