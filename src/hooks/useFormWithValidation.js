bc71ae9ed40e5e6766326ea5269e354566ab3d10
Anna <thoria@yandex.ru>
2023-11-07 18:15:34 +0300

    add space and '-' to names regex

--- src/hooks/useFormWithValidation.js
+++ src/hooks/useFormWithValidation.js
@@ -38,7 +38,7 @@ export const useFormWithValidation = () => {
 
   const _isValidName = (name) => {
     name = name.trim();
-    const nameRegex = /^[А-Яа-яё]+$/;
+    const nameRegex = /^[А-Яа-яё\s\-]+$/;
     if (!nameRegex.test(name)) return 'Только кириллица';
     if (name.length < 1 || name.length > 50) {
       return 'Имя должно содержать не меньше 1 и не больше 50 букв';
