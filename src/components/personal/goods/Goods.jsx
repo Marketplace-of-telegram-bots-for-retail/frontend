f0cfe78c8b808c4408b3fae95ede99684d647f63
Ekaterina Shreyner <kkkatya88@yandex.ru>
2023-11-09 02:07:44 +0400

    add GoodBots and fix CartCard

--- src/components/personal/goods/Goods.jsx
+++ src/components/personal/goods/Goods.jsx
@@ -1,13 +1,23 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { getCartData } from '../../../store';
 import './Goods.css';
 import GoodsNewBot from './GoodsNewBot/GoodsNewBot';
+import CartCard from '../../Cart/CartCard/CartCard';
 
 const Goods = () => {
   const [isShown, setIsShown] = useState(false);
+  const [isShownGoods, setIsShownGoods] = useState(true);
+  const { items } = useSelector(getCartData);
 
   function handleAddClick() {
     setIsShown(!isShown);
+    setIsShownGoods(!isShownGoods);
+  }
+  function handleEditBot() {
+    setIsShown(true);
+    setIsShownGoods(false);
   }
 
   return (
@@ -20,9 +30,13 @@ const Goods = () => {
       >
         Добавить новый
       </button>
-      {!isShown && (
-        <GoodsNewBot />
-      )}
+      {isShown && <GoodsNewBot />}
+      {isShownGoods &&
+        items.map((item) => {
+          return (
+            <CartCard key={item.id} item={item} setIsShown={handleEditBot} />
+          );
+        })}
     </section>
   );
 };
