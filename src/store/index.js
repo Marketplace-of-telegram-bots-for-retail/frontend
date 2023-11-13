917a43ddbdfd5353a66ddafdb45953b52caa8478
OksanaKam <moremore1@yandex.ru>
2023-11-13 08:21:46 +0200

    [/] add logic new bot

--- src/store/index.js
+++ src/store/index.js
@@ -7,7 +7,6 @@ import userOrdersDataReducer from './userOrdersDataSlice';
 import userDataReducer from './userSlice';
 import sellersProductsReducer from './sellersProductsSlice';
 
-
 export default configureStore({
   reducer: {
     searchFormData: searchFormDataReducer,
@@ -27,5 +26,6 @@ export {
   getSearchFormData,
   getUserOrdersData,
   getUserData,
+  getSellersProducts,
 } from './selectors';
 export * as selectors from './selectors';
