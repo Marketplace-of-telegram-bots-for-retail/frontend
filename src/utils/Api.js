3ee3de899eade28cc0522ab6c7e2dd68247d3444
Виктор Тузовский <vtuzovskiy@fura.ru>
2023-11-29 23:38:23 +0700

    Changes for bugaton4

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -215,7 +215,7 @@ class Api {
 }
 
 const config = {
-  baseUrl: 'https://botmarketplace.ru/api',
+  baseUrl: 'https://bugaton4.acceleratorpracticum.ru/api',
   headers: {
     'Content-Type': 'application/json',
   },
