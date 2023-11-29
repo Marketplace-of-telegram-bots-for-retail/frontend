56eb2b974c3b575628e46ad0c680b07bc26b4f28
Виктор Тузовский <vtuzovskiy@fura.ru>
2023-11-30 00:18:18 +0700

    Restore changes

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -215,7 +215,7 @@ class Api {
 }
 
 const config = {
-  baseUrl: 'https://bugaton4.acceleratorpracticum.ru/api',
+  baseUrl: 'https://botmarketplace.ru/api',
   headers: {
     'Content-Type': 'application/json',
   },
